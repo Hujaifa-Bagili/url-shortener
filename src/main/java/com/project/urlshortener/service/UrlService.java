6920195c24fce1c9b07ed309db1d049f58421220
Hujaifa-Bagili <hujaifa857@gamil.com>
2026-02-22 14:26:36 +0530

    Update BASE_URL to include https and trailing slash

--- src/main/java/com/project/urlshortener/service/UrlService.java
+++ src/main/java/com/project/urlshortener/service/UrlService.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 @Service
 public class UrlService {
     private static final String BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String BASE_URL ="https://url-shortener-production-4d07.up.railway.app/";
+    private static final String BASE_URL ="url-shortener-production-8d8b.up.railway.app";
 
     @Autowired
     private UrlRepository urlRepository;
